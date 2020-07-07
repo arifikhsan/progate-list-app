dc7fcb548bef31ec8fdbbb2d5731fb4aeac88788
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-07 09:02:37 +0700

    select

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql");
 const app = express();
 
-app.get('/', (req, res) => {
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "progate_list_app",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log('successfully connected to mysql')
+});
+
+app.get("/", (req, res) => {
+  connection.query('SELECT * FROM items', (error, results) => {
+    console.log(results);
     res.render('hello.ejs');
+  });
 });
 
 app.listen(3000);
